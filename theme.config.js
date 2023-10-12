8568d52810083dcf8da3d97d92262aad872a2ea4
Kyle Ancheta <kyleancheta@live.com>
2023-10-12 01:12:01 -0700

    hehe

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      {YEAR} © Kyle Ancheta.
+      {YEAR} © Kyle Ancheta. Built with anxiety.
       {/* <a href="/feed.xml">RSS</a> */}
       <style jsx>{`
         a {
